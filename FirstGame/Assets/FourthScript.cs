3318a6c4f722a413c610f96ebaef7e29cbfcd3ce
Unknown <DavisTrevor91@gmail.com>
2018-01-16 11:18:08 -0700

    Did in class assignment with scripts.

---
+++ FirstGame/Assets/FourthScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class is an object classification.
+//You must have open and closed {} per object.
+public class FourthScript : MonoBehaviour {
+
+	//Void is a function that is a void or empty or null.
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
