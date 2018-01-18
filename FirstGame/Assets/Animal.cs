3dd206cfbed629972cbcc7123fd59617d1ce523c
Unknown <DavisTrevor91@gmail.com>
2018-01-18 11:15:02 -0700

    Did work on Animal class, removed some empty scripts.

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdScript : MonoBehaviour {
+public class Animal : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
 	}
 	
+	void OnMouseDown()
+	{
+		print("Meow.");
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
