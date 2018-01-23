1e6dc097d1faa6f0c0181bb1948ecc1115bff33b
Unknown <DavisTrevor91@gmail.com>
2018-01-23 11:06:52 -0700

    Added notes about syntax and convention to Animal.cs.

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Classes are used to classify things, they're used to seperate objects.
+//Parantheses are used to tell classes what to do.
+//Curly brackets are a box within which the classes exist.
+//The conventions and syntax in C# are both used to turn the language of computers
+//into English, so that we as humans can speak plainly to get the computer to
+//do what we want it to do.
+
 public class Animal : MonoBehaviour {
 
 	// Use this for initialization
