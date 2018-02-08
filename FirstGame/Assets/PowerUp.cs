9ac25e947f0e0ecf2af8614889ff0a1806fd0084
Unknown <DavisTrevor91@gmail.com>
2018-02-08 11:19:01 -0700

    Math Operators and Scriptable Objects

--- FirstGame/Assets/PowerUp.cs
+++ FirstGame/Assets/PowerUp.cs
@@ -8,6 +8,12 @@ public class PowerUp : ScriptableObject {
 
 	public int PowerLevel = 100;
 	public string Name = "Health";
+	public Player CurrentPlayer;
 
-
+	public void RunPowerUp()
+	{
+		CurrentPlayer.Score += PowerLevel;
+		//Adds Value
+		Debug.Log(PowerLevel);
+	}
 }
