8253e11312a836807c4e9594b3b70f31d4efade9
Unknown <DavisTrevor91@gmail.com>
2018-02-15 11:15:14 -0700

    Added Incredibles loops and scriptable objects.

---
+++ FirstGame/Assets/Lists.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour {
+
+public string AddMember;
+public TheIncredibles TheIncredibles;
+
+
+	// Use this for initialization
+	void Start () {
+		TheIncredibles.FamilyList.Add(AddMember);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
