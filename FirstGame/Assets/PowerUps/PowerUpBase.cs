2cd875a26307e3772d8bcc24d68a16a1e10c8881
Unknown <DavisTrevor91@gmail.com>
2018-02-20 11:11:32 -0700

    Added Switch Statements and PowerUpBase.

---
+++ FirstGame/Assets/PowerUps/PowerUpBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUpBase : ScriptableObject {
+
+	public int PowerLevel;
+
+
+}
---
+++ FirstGame/Assets/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	public string UserName;
+
+	// Use this for initialization
+	void Start () {
+		switch (UserName)
+		{
+			case "Password":
+				print("Password is correct.");
+				//Do Work
+				break;
+			
+			case "LevelUp":
+				print ("Ding!");
+				//Do Work
+				break;
+			
+			case "BlackPanther":
+				print ("Made all of the money.");
+				break;
+
+			default:
+				print("Why don't you just tell me which movie you want to see");
+				//Do Work
+			break;
+
+		}
+	}
+	
+}
