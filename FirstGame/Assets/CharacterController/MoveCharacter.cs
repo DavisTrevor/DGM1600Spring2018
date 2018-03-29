86fb487bf24bb3b5c53a7186843953643639ffdc
Unknown <DavisTrevor91@gmail.com>
2018-03-29 11:09:48 -0600

    Added some movement functionality and removed calculator.

---
+++ FirstGame/Assets/CharacterController/MoveCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+	
+	public MovePattern MovePattern;
+	private CharacterController controller;
+
+	void Start () 
+	{
+		controller = GetComponent<ChatacterController>();
+	}
+	
+	void Update () 
+	{
+		MovePattern.Move(controller, transform);
+	}
+}
