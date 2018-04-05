4f5ad5e4be70f0452ece75ea0129583fcf89dc66
Unknown <DavisTrevor91@gmail.com>
2018-04-05 11:06:54 -0600

    Added BoolInput for jumping.

---
+++ FirstGame/Assets/CharacterController/BoolInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BoolInput")]
+public class BoolInput : InputBase {
+
+	public string InputName = "Jump";
+	public float FloatValue = 1;
+
+	public override float SetFloat()
+	{
+		return Input.GetButton(InputName) ? FloatValue : 0;
+	}
+
+}
